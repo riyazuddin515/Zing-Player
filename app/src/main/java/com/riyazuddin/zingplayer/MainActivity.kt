03e91a09d1a8d9bfe5ee9a0e1bf9e2cdee3fb543
Riyazuddin Mohammed <riyazuddin515@gmail.com>
2022-04-14 14:04:14 +0530

    Thumbnail upload implemented

--- app/src/main/java/com/riyazuddin/zingplayer/MainActivity.kt
+++ app/src/main/java/com/riyazuddin/zingplayer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.riyazuddin.zingplayer
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
@@ -19,8 +20,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            listAll()
+        val button = findViewById<Button>(R.id.btnUpload)
+        button.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                listAll()
+            }
+        }
+
+        val setThumbnailBtn = findViewById<Button>(R.id.btnSetThumbnails)
+        setThumbnailBtn.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                setThumbnails()
+            }
         }
     }
 
@@ -28,22 +39,47 @@ class MainActivity : AppCompatActivity() {
         val songsDB = FirebaseFirestore.getInstance().collection("songs")
         val songStorageRef = Firebase.storage.getReference("songs")
         val listResult = songStorageRef.listAll().await()
-        var c = 0
+        var c = 1
         listResult.items.forEach { storageRef ->
             val url = storageRef.downloadUrl.await()
             val name = storageRef.name.lowercase().removeSuffix(".mp3").capitalize()
-            val song = Song((++c).toString(), name, url.toString(), "")
             val dId = if (c < 10) "media0$c" else "media$c"
+            val song = Song(dId, name, url.toString(), "")
             songsDB.document(dId).set(song).await()
             Log.d(TAG, "Success $c")
+            c++
+        }
+    }
+
+    private suspend fun setThumbnails() {
+        val songsDB = FirebaseFirestore.getInstance().collection("songs")
+        val songList = songsDB.get().await().toObjects(Song::class.java)
+        val thumbnailStorageRef = Firebase.storage.getReference("thumbnails")
+        val thumbnailList = thumbnailStorageRef.listAll().await()
+        var c = 0
+        thumbnailList.items.forEach {
+            val name = it.name.lowercase().removeSuffix(".jpeg").trim()
+            val song = songList.filter { song ->
+                song.title.lowercase().trim() == name
+            }
+            if (song.isNotEmpty()) {
+                val url = it.downloadUrl.await().toString()
+                song[0].imageUrl = url
+                Log.i(TAG, "setThumbnails: ${song[0]}")
+                Log.i(TAG, "setThumbnails: <---------------------${++c}---------------------------->")
+                songsDB.document(song[0].mediaId).update("imageUrl", url)
+            }else{
+                Log.e(TAG, "setThumbnails: NOT FOUND -> $name", )
+            }
+
         }
     }
 
     data class Song(
-        val mediaId: String,
-        val title: String,
-        val songUrl: String,
-        val imageUrl: String
+        val mediaId: String = "",
+        val title: String = "",
+        val songUrl: String = "",
+        var imageUrl: String = ""
     )
 
 
